c50eb5b6c25039b070608028d1e949373f4ebd84
Kirill <kirillsm05@gmail.com>
2025-03-05 13:42:20 +0700

    Add files via upload

---
+++ introduction-to-IT/lab1/task1/lab1_task1_5.asm
@@ -0,0 +1,29 @@
+ORG 100h
+
+val_a EQU 2
+val_b EQU -1
+val_c EQU 5
+bit_mask EQU 0111111111111110b
+bit_rotate EQU 2
+
+DIV_CONST DW 2
+
+main:
+    PUSH BP
+    MOV BP, SP
+    
+    MOV AX, val_a 
+    MOV DI, val_b
+    MOV CX, val_c
+    
+    SUB DI, CX
+    IMUL CX
+    DEC DI
+    IDIV DIV_CONST
+    AND AX, bit_mask
+    ROL AX, bit_rotate
+
+    MOV SP, BP
+    POP BP
+    RET
+END
